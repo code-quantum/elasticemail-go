412213176addd3c001a9877324e30a3e41e8f5d3
Alexey Kurgansky <letchiiik@gmail.com>
2018-10-15 16:26:34 +0700

    init

---
+++ api_email.go
@@ -0,0 +1,51 @@
+package elasticemail
+
+import (
+	"fmt"
+	"github.com/google/go-querystring/query"
+	"log"
+	"net/http"
+	"net/url"
+	"reflect"
+	"strconv"
+)
+
+type GetEmailStatusParams struct {
+	TransactionID    string `json:"transactionID" url:"transactionID"`
+	ShowAbuse        bool   `json:"showAbuse" url:"showAbuse"`
+	ShowClicked      bool   `json:"showClicked" url:"showClicked"`
+	ShowDelivered    bool   `json:"showDelivered" url:"showDelivered"`
+	ShowErrors       bool   `json:"showErrors" url:"showErrors"`
+	ShowFailed       bool   `json:"showFailed" url:"showFailed"`
+	ShowMessageIDs   bool   `json:"showMessageIDs" url:"showMessageIDs"`
+	ShowOpened       bool   `json:"showOpened" url:"showOpened"`
+	ShowPending      bool   `json:"showPending" url:"showPending"`
+	ShowSent         bool   `json:"showSent" url:"showSent"`
+	ShowUnsubscribed bool   `json:"showUnsubscribed" url:"showUnsubscribed"`
+}
+
+// Get email batch status
+// Access Level required: ViewReports
+func (m *ElasticEmailImpl) GetEmailStatus(params GetEmailStatusParams) {
+
+	req, err := m.client.NewRequest("GET", m.apiBase, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	v, _ := query.Values(params)
+	v.Add("apikey", m.apiKey)
+	req.URL.RawQuery = v.Encode()
+	fmt.Println(req.URL.String())
+
+	resp, _ := m.client.Do(r)
+
+	f, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Println(err)
+	}
+	resp.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(f))
+}
---
+++ elasticemail.go
@@ -0,0 +1,52 @@
+package elasticemail
+
+import (
+	"net/http"
+)
+
+const (
+	ApiBase       = "https://api.elasticemail.com/v2"
+	emailEndpoint = "email"
+)
+
+type ElasticEmail interface {
+	APIBase() string
+	APIKey() string
+	Client() *http.Client
+	SetClient(client *http.Client)
+	GetEmailStatusParams(params GetEmailStatusParams)
+}
+
+type ElasticEmailImpl struct {
+	apiBase string
+	apiKey  string
+	client  *http.Client
+}
+
+func NewElasticEmail(apiKey string) *ElasticEmailImpl {
+	return &ElasticEmailImpl{
+		apiBase: ApiBase,
+		apiKey:  apiKey,
+		client:  http.DefaultClient,
+	}
+}
+
+// Client returns the HTTP client configured for this client.
+func (m *ElasticEmailImpl) Client() *http.Client {
+	return m.client
+}
+
+// SetClient updates the HTTP client for this client.
+func (m *ElasticEmailImpl) SetClient(c *http.Client) {
+	m.client = c
+}
+
+// ApiBase returns the API Base URL configured for this client.
+func (m *ElasticEmailImpl) APIBase() string {
+	return m.apiBase
+}
+
+// SetAPIBase updates the API Base URL for this client.
+func (m *ElasticEmailImpl) SetAPIBase(address string) {
+	m.apiBase = address
+}
